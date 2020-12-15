02610416f503cf6dca343e6918331a4333e33d2e
awest <awest@fluentstream.com>
2020-12-15 16:20:47 -0600

    finalizing hello app

--- lib/cdk-hello-stack.ts
+++ lib/cdk-hello-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CdkHelloStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    // defines a API gateway REST API resource
+    new apigateway.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
   }
 }
